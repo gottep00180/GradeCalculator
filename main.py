5355fd41b082911d4566d9abca8e87525521cf5e
gottep00180 <58423093+gottep00180@users.noreply.github.com>
2021-07-18 20:16:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
+#รับค่าคะแนนแต่ละวิชามา
 d1 = int(input("english: "))
 d2 = int(input("maths: "))
 d3 = int(input("thai: "))
 d4 = int(input("physics: "))
 d5 = int(input("chemistry: "))
-
+#หาค่าเฉลี่ย(Mean)
 mean = (d1+d2+d3+d4+d5) / 5
-
+#แสดงค่าคะแนนรายวิชา
+#หมายเหตุ: หากยังไม่ถูกต้องลองตรวจสอบเว้นวรรคดู
 print("English score ="+str(d4))
 print("Maths score ="+str(d2))
 print("Thai score ="+str(d3))
 print("Physics score ="+str(d4))
 print("Chemistry score ="+str(d5))
-
+#แสดงค่าเฉลี่ย
 print("Mean = "+str(mean))
-
+#แสดงค่าในรูปแบบ %
+#การนำไปหาร1เพื่อให้ได้ทศนิยม 1 ตำแหน่ง
+#หมายเหตุ: หากยังไม่ถูกต้องลองตรวจสอบเว้นวรรคดู
 print("English = "+str((d1/1))+"%")
 print("Maths = "+str((d2/1))+"%")
 print("Thai = "+str((d3/1))+"%")
