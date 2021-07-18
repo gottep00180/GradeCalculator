f32f74fe39830605cbb2057c8068201b87f75f98
gottep00180 <58423093+gottep00180@users.noreply.github.com>
2021-07-18 20:13:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+d1 = int(input("english: "))
+d2 = int(input("maths: "))
+d3 = int(input("thai: "))
+d4 = int(input("physics: "))
+d5 = int(input("chemistry: "))
+
+mean = (d1+d2+d3+d4+d5) / 5
+
+print("English score ="+str(d4))
+print("Maths score ="+str(d2))
+print("Thai score ="+str(d3))
+print("Physics score ="+str(d4))
+print("Chemistry score ="+str(d5))
+
+print("Mean = "+str(mean))
+
+print("English = "+str((d1/1))+"%")
+print("Maths = "+str((d2/1))+"%")
+print("Thai = "+str((d3/1))+"%")
+print("Physics ="+str((d4/1))+"%")
+print("Chemistry ="+str((d5/1))+"%")
